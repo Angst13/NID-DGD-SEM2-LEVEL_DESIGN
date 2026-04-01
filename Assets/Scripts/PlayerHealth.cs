5b7edb1fb27797768da98199b7a2baa319d7b08f
Angst13 <aashishdev.raju@gmail.com>
2026-04-01 18:47:52 +0530

    commit 3

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,22 +6,67 @@ public class PlayerHealth : MonoBehaviour
 {
     public int maxHits = 3;
     public float invincibleTime = 1.5f;
+    public float deathAnimationDuration = 1.5f;
 
-    private int hitCount = 0;
-    private Vector2 startPosition;
+    // Static variables persist across scene loads without singleton
+    private static int hitCount = 0;
+    private static bool isDead = false;
+    private static int respawnScene = 0;
+    private static Vector2 respawnPosition;
+
     private bool isInvincible = false;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private const string HURT_TRIGGER = "Hurt";
+    private const string DEAD_TRIGGER = "Dead";
 
     void Start()
     {
-        startPosition = transform.position;
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Find SpawnPoint in this scene
+        GameObject spawnPoint = GameObject.FindWithTag("Respawn");
+        if (spawnPoint != null)
+        {
+            transform.position = spawnPoint.transform.position;
+            respawnPosition = spawnPoint.transform.position;
+        }
+        else
+        {
+            respawnPosition = transform.position;
+            Debug.LogWarning("No SpawnPoint found in scene: " + gameObject.scene.name);
+        }
+
+        respawnScene = SceneManager.GetActiveScene().buildIndex;
+
+        // If returning after death, finish respawn
+        if (isDead)
+        {
+            StartCoroutine(FinishRespawn());
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("Enemy") && !isInvincible)
-        {
+        if (isDead || isInvincible) return;
+
+        if (col.gameObject.CompareTag("Boss"))
+            StartCoroutine(HandleDeath());
+        else if (col.gameObject.CompareTag("Enemy"))
             TakeHit();
-        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isDead || isInvincible) return;
+
+        if (other.CompareTag("Boss"))
+            StartCoroutine(HandleDeath());
+        else if (other.CompareTag("Enemy"))
+            TakeHit();
     }
 
     void TakeHit()
@@ -29,26 +75,76 @@ public class PlayerHealth : MonoBehaviour
         Debug.Log("Hit! " + hitCount + " / " + maxHits);
 
         if (hitCount >= maxHits)
-        {
-            GameOver();
-        }
+            StartCoroutine(HandleDeath());
         else
         {
-            transform.position = startPosition;
+            animator.SetTrigger(HURT_TRIGGER);
             StartCoroutine(InvincibleCooldown());
         }
     }
 
-    System.Collections.IEnumerator InvincibleCooldown()
+    IEnumerator HandleDeath()
+    {
+        isDead = true;
+        isInvincible = true;
+
+        // Save scene and spawn position before reloading
+        respawnScene = SceneManager.GetActiveScene().buildIndex;
+
+        GameObject spawnPoint = GameObject.FindWithTag("Respawn");
+        if (spawnPoint != null)
+            respawnPosition = spawnPoint.transform.position;
+        else
+            respawnPosition = transform.position;
+
+        animator.SetTrigger(DEAD_TRIGGER);
+
+        yield return new WaitForSeconds(deathAnimationDuration);
+
+        SceneManager.LoadScene(respawnScene);
+    }
+
+    IEnumerator FinishRespawn()
+    {
+        yield return null;
+
+        hitCount = 0;
+        isDead = false;
+        isInvincible = false;
+
+        animator.ResetTrigger(HURT_TRIGGER);
+        animator.ResetTrigger(DEAD_TRIGGER);
+        animator.Rebind();
+        animator.Update(0f);
+
+        spriteRenderer.enabled = true;
+        transform.position = respawnPosition;
+    }
+
+    IEnumerator InvincibleCooldown()
     {
         isInvincible = true;
-        yield return new WaitForSeconds(invincibleTime);
+
+        float elapsed = 0f;
+        while (elapsed < invincibleTime)
+        {
+            spriteRenderer.enabled = !spriteRenderer.enabled;
+            yield return new WaitForSeconds(0.1f);
+            elapsed += 0.1f;
+        }
+        spriteRenderer.enabled = true;
+
         isInvincible = false;
     }
 
-    void GameOver()
+    public void FullReset()
     {
-        Debug.Log("GAME OVER!");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        hitCount = 0;
+        isDead = false;
+        isInvincible = false;
+        respawnScene = 0;
     }
+
+    public int GetHitCount() => hitCount;
+    public bool GetIsDead() => isDead;
 }
